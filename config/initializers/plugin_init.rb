a9bae07e82ffacf4a206842f4211fbf5a302224e
Kasun Thennakoon <tmkasun@gmail.com>
2014-06-13 14:31:29 +0530

    place Disk-Wizard under disk tab named as add subtab

--- config/initializers/plugin_init.rb
+++ config/initializers/plugin_init.rb
@@ -1,6 +1,6 @@
 # plugin initialization
-t = Tab.new("disk_wizards", "disk_wizards", "/tab/disk_wizards")
-# add any subtabs with what you need. params are controller and the label, for example
-t.add("index", "details")
-t.add("settings", "settings")
-t.add("advanced", "advanced")
+unless t = Tab.find("disks")
+    t = Tab.new("disks", "disks", "/tab/disks")
+end
+
+t.add("select_device", "Add")
