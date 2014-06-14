a42f8f7209bf644f053bf4de9d009c0d6ee82544
Kasun Thennakoon <tmkasun@gmail.com>
2014-06-14 16:29:53 +0530

    Remove unnecessary migration files

--- db/migrate/20140509194909_create_settings.rb
+++
@@ -1,9 +0,0 @@
-class CreateSettings < ActiveRecord::Migration
-  def change
-    create_table :settings ,:force => true do |t|
-      t.string "name"
-      t.string "value"
-      t.string "kind",  :default => "general"
-    end
-  end
-end
