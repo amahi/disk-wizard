b9d844610c98403e79a32f020aacb7b98a8c5f57
Kasun Thennakoon <tmkasun@gmail.com>
2014-06-16 00:57:55 +0530

    Add exception handler and error logger for disk-wizard plugin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,13 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   include Chartkick::Helper
+  rescue_from Exception, :with => :dw_excepton_handler
+
+  protected
+
+  def dw_excepton_handler(exception)
+    session[:exception] = exception.inspect
+    redirect_to(defined?(disk_wizards_engine) ? disk_wizards_engine.error_path : error_path)
+  end
 
 end
