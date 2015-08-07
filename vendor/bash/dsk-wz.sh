206e12898ee0df6df6db83f5ca6f3c239b299968
Ahmed Kamal <ahmed_kamal1432@yahoo.com>
2015-08-07 03:00:36 +0300

    change exec to eval to be able to execute command with single qoute

--- vendor/bash/dsk-wz.sh
+++ vendor/bash/dsk-wz.sh
@@ -9,11 +9,11 @@ write_log()
   while read text
   do
 			if [ "$DEBUG_MODE" = true ]; then
-					LOGTIME=`date "+%Y-%m-%d %H:%M:%S"`
-      		LOG="dsw.log"
-    			touch $LOG
-        		if [ ! -f $LOG ]; then echo "ERROR!! Cannot create log file $LOG. Exiting."; exit 1; fi
-    			echo $LOGTIME": $text" | tee -a $LOG;
+				LOGTIME=`date "+%Y-%m-%d %H:%M:%S"`
+				LOG="dsw.log"
+				touch $LOG
+				if [ ! -f $LOG ]; then echo "ERROR!! Cannot create log file $LOG. Exiting."; exit -1; fi
+				echo $LOGTIME": $text" >> $LOG;
 			fi
   done
 }
@@ -22,14 +22,14 @@ executor(){
 	local command=$1;
 	shift;
 	local params="$*";
-	# echo  "Start executing $command with arguments = $params" | write_log;
-	exec sudo $command $params #>> dsw.log 2>&1;
+	echo  "Start executing $command with arguments = $params" | write_log;
+	eval sudo $command $params;
 }
 
 if [ $# -lt 2 ]
 	then
 		echo -e "An insufficient number of arguments(arguments)" | write_log ;
-		exit 1;
+		exit -1;
 fi
 
 command=$1;
@@ -53,8 +53,6 @@ fdisk)
 *)
 	executor $command $arguments;
 	# if a command is not one we know, we exit with an error
-	echo "Sorry, command $command is not known";
-	exit 1;
 ;;
 esac
-exit 1;
+exit 0;
