975b8cd3a10a03a996954c649cda06e0b064c15f
sukhbir-singh <sukhbir947@gmail.com>
2019-08-10 04:20:33 +0530

    update debugging script for fpaste url

--- vendor/bash/debug_info.sh
+++ vendor/bash/debug_info.sh
@@ -1,36 +1,46 @@
 #!/bin/bash
 echo "Disk-Wizard Debug information fetcher"
 
-echo -e "\n------------This Debug Information was Generated on $(date +"%B-%d-%Y %r")------------\n" > /var/log/debug_info.tmp
+output=/tmp/dw_debug_$$.tmp
 
-printf "\n/*===*dsw.log wrapper script log (tail -n 50)===*/\n" >> /var/log/debug_info.tmp
+#exec > $output 2>&1
+
+echo -e "\n------------This Debug Information was Generated on $(date +"%B-%d-%Y %r")------------\n"
+
+printf "\n/*===*dsw.log wrapper script log (tail -n 50)===*/\n"
+
 if [ ! -f /var/hda/apps/520ut3lo6w/elevated/dsw.log ]; then
-	        echo "File not found!" >> /var/log/debug_info.tmp
-	else
-		        tail -n 50 /var/hda/apps/520ut3lo6w/elevated/dsw.log >> /var/log/debug_info.tmp
-		fi
+	echo "File not found!"
+else
+	tail -n 50 /var/hda/apps/520ut3lo6w/elevated/dsw.log
+fi
 
-		printf "\n/*=== *Current mounted partitions ===*/\n" >> /var/log/debug_info.tmp
-		sudo mount -l >> /var/log/debug_info.tmp 2>&1
+printf "\n/*=== *Current mounted partitions ===*/\n"
+sudo mount -l
 
-		printf "\n/*=== *list disk/partitions details with parted ===*/\n" >> /var/log/debug_info.tmp
-		parted -sl print all  >> /var/log/debug_info.tmp 2>&1
+printf "\n/*=== *list disk/partitions details with parted ===*/\n"
+parted -sl print all
 
-		printf "\n/*=== *List all logical volumes ===*/\n" >> /var/log/debug_info.tmp
-		lvdisplay -av >> /var/log/debug_info.tmp 2>&1
+printf "\n/*=== *List all logical volumes ===*/\n"
+lvdisplay -av
 
-		printf "\n/*=== *dsk-wz.sh wrapper script ===*/\n" >> /var/log/debug_info.tmp
-		if [ ! -f /var/hda/apps/520ut3lo6w/elevated/dsk-wz.sh ]; then
-			        echo "File not found!" >> /var/log/debug_info.tmp
-			else
-				        cat /var/hda/apps/520ut3lo6w/elevated/dsk-wz.sh >> /var/log/debug_info.tmp
-				fi
+printf "\n/*=== *dsk-wz.sh wrapper script ===*/\n"
 
-				printf "\n/*=== *disk-wizard rails log file(tail -n 50)===*/\n" >> /var/log/debug_info.tmp
-				if [ ! -f /var/hda/platform/html/log/dw_debug.log ]; then
-					        echo "File not found!" >> /var/log/debug_info.tmp
-					else
-						        tail -n 50 /var/hda/platform/html/log/dw_debug.log >> /var/log/debug_info.tmp
-						fi
+if [ ! -f /var/hda/apps/520ut3lo6w/elevated/dsk-wz.sh ]; then
+	echo "File not found!"
+else
+	cat /var/hda/apps/520ut3lo6w/elevated/dsk-wz.sh
+fi
 
-						fpaste -l bash /var/log/debug_info.tmp
+printf "\n/*=== *disk-wizard rails log file(tail -n 50)===*/\n"
+
+if [ ! -f /var/hda/platform/html/log/dw_debug.log ]; then
+	echo "File not found!"
+else
+	tail -n 50 /var/hda/platform/html/log/dw_debug.log
+fi
+
+#fpaste -l "shell" /var/log/debug_info.tmp
+
+tail -n 600 /var/hda/platform/html/log/dw_debug.log > temp_file && fpaste temp_file
+
